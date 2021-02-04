86d1dc6392872a5ddbce27a561589935ab19e8f3
LHG <LHG@DESKTOP-RP9L057>
2021-02-04 17:19:42 +0800

    封装函数

--- text1/Form1.cs
+++ text1/Form1.cs
@@ -19,20 +19,44 @@ namespace text1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            double total = buy(1.9, 6, 0.75);
+            MessageBox.Show(total.ToString());
+        }
+        //*****************************封装函数******************************
+        public double buy(double price, double number, double discount)
+        {
+            double total;          //设置总价的变量
+            //计算总价钱
+            total = number * price;
+            //计算整十的数
+            int n = (int)(total / 10);
 
+            //计算整十打折后的得数
+            //给打折后的整十设置变量
+            double zhe = 0;
+            //计算整十打折后的得数
+            for (int i = 0; i < n; i++)
+            {
+                zhe = zhe + 10 * discount;
+            }
+            //取余
+            double y = total % 10;
+            //整十打折后的得数加余数
+            total = zhe + y;
+            return total;
         }
-        public void Intermediary(string inte)
-        {
-            Console.WriteLine(inte);
-        }
-        public string PostOffice(int area, int price)
-        {
-            return "张三想买这套房，愿意出价" + (area * price - 1) + "元";
-        }
+        //public void Intermediary(string inte)
+        //{
+        //    Console.WriteLine(inte);
+        //}
+        //public string PostOffice(int area, int price)
+        //{
+        //    return "张三想买这套房，愿意出价" + (area * price - 1) + "元";
+        //}
 
-        public void Football()
-        {
-            Console.WriteLine("梅西会打篮球");
-        }
+        //public void Football()
+        //{
+        //    Console.WriteLine("梅西会打篮球");
+        //}
     }
 }
